ca504a1e0352fdaaf07dd82336815c056df03d7c
Bruno Montebello <brunormontebello@hotmail.com>
2021-02-22 22:47:17 +0000

    Create create-post + style

---
+++ social-media-app/src/containers/create-post/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SignInBtn } from '../../components';
+
+export default function CreatePost(props) {
+  return (
+    <div className="createPost">
+      <SignInBtn/>
+      <p>to post and comment</p>
+    </div>
+  );
+}
