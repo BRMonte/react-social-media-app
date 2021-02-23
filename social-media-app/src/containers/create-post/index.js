8d1e09d14e577b8ca96236a90aab4a7ed2b8e2f0
Bruno Montebello <brunormontebello@hotmail.com>
2021-02-23 20:51:34 +0000

    Create Post box with hook

--- social-media-app/src/containers/create-post/index.js
+++ social-media-app/src/containers/create-post/index.js
@@ -1,15 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './style.css';
 import { SignInBtn } from '../../components';
 import { UserContext } from "../../contexts/user";
 
 export default function CreatePost() {
   const [user, setUser] = useContext(UserContext).user;
+  const [caption, setCaption] = useState("");
   return (
     <div className="createPost">
       {
         user ?
-        (<p>Create Post</p>)
+        (<div className="createPost__loggedIn">
+          <p>Create Post</p>
+          <div className="createPost__loggedInCenter">
+            <textarea className="createPost__textArea" rows="3" value={caption} onChange={(e) => setCaption(e.target.value)}>
+
+            </textarea>
+          </div>
+        </div>)
         :
         (<div>
           <SignInBtn/>
