d94aab9d135c95ef4f29da4587d012af13df1f63
Bruno Montebello <brunormontebello@hotmail.com>
2021-02-23 20:23:31 +0000

    Config to sign-in btn not to be shown for signed-in users

--- social-media-app/src/containers/create-post/index.js
+++ social-media-app/src/containers/create-post/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './style.css';
 import { SignInBtn } from '../../components';
+import { UserContext } from "../../contexts/user";
 
-export default function CreatePost(props) {
+export default function CreatePost() {
+  const [user, setUser] = useContext(UserContext).user;
   return (
     <div className="createPost">
-      <SignInBtn/>
-      <p>to post and comment</p>
+      {
+        user ?
+        (<p>Create Post</p>)
+        :
+        (<div>
+          <SignInBtn/>
+          <p style={{ marginLeft: "12px" }}>to Post & Comment</p>
+        </div>)
+      }
     </div>
   );
 }
