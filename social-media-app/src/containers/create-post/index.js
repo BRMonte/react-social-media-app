2b6122c8804e732d1e350214ff002d117563450d
Bruno Montebello <brunormontebello@hotmail.com>
2021-02-23 21:34:03 +0000

    Add material design + Create add image to post functionality

--- social-media-app/src/containers/create-post/index.js
+++ social-media-app/src/containers/create-post/index.js
@@ -2,10 +2,16 @@ import React, { useContext, useState } from 'react';
 import './style.css';
 import { SignInBtn } from '../../components';
 import { UserContext } from "../../contexts/user";
+import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
 
 export default function CreatePost() {
   const [user, setUser] = useContext(UserContext).user;
   const [caption, setCaption] = useState("");
+
+  const handleChange = () => {
+
+  };
+
   return (
     <div className="createPost">
       {
@@ -13,9 +19,22 @@ export default function CreatePost() {
         (<div className="createPost__loggedIn">
           <p>Create Post</p>
           <div className="createPost__loggedInCenter">
-            <textarea className="createPost__textArea" rows="3" value={caption} onChange={(e) => setCaption(e.target.value)}>
-
-            </textarea>
+            <textarea
+              className="createPost__textArea"
+              rows="3" value={caption}
+              onChange={(e) => setCaption(e.target.value)}
+            />
+          </div>
+          <div className="createPost__imageUpload">
+          <label htmlFor="fileInput">
+            <AddAPhotoIcon style={{cursor: "pointer", fontSize: "20px"}}/>
+          </label>
+            <input
+              id="fileInput"
+              type="file"
+              accept="image/*"
+              onChange={handleChange}
+            />
           </div>
         </div>)
         :
