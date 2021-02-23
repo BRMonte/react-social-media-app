ec9daa8dcff35e488bca0c5eced62e293c20f828
Bruno Montebello <brunormontebello@hotmail.com>
2021-02-23 17:25:15 +0000

    Create user context + display profile img icon

--- social-media-app/src/containers/navbar/index.js
+++ social-media-app/src/containers/navbar/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import './style.css';
 import { SignInBtn } from '../../components'
+import { UserContext } from '../../contexts/user';
 
 export default function Navbar() {
+  const [user, setUser] = useContext(UserContext).user;
+
   return (
     <div className="navbar">
       <p>Social Media</p>
-      <SignInBtn/>
+      {user ? (<img className="navbar__img" src={user.photoURL}></img>) : (<SignInBtn/>)}
     </div>
   );
 }
