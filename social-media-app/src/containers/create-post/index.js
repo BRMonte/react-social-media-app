4925061ce2c47a391bc0ed2c979eb32250ad4543
Bruno Montebello <brunormontebello@hotmail.com>
2021-02-23 23:58:15 +0000

    Add style to image appended on post

--- social-media-app/src/containers/create-post/index.js
+++ social-media-app/src/containers/create-post/index.js
@@ -7,8 +7,19 @@ import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
 export default function CreatePost() {
   const [user, setUser] = useContext(UserContext).user;
   const [caption, setCaption] = useState("");
+  const [image, setImage] = useState(null);
 
   const handleChange = () => {
+    if (e.target.files[0]){
+      setImage(e.target.files[0]);
+      let imagePreview = document.getElementById('image_preview');
+      let selectedImageSrc = URL.createObjectURL(e.target.files[0]);
+      imagePreview.src = selectedImageSrc;
+      imagePreview.style.display = "block";
+    }
+  };
+
+  const handleUpload = () => {
 
   };
 
@@ -25,16 +36,27 @@ export default function CreatePost() {
               onChange={(e) => setCaption(e.target.value)}
             />
           </div>
-          <div className="createPost__imageUpload">
-          <label htmlFor="fileInput">
-            <AddAPhotoIcon style={{cursor: "pointer", fontSize: "20px"}}/>
-          </label>
-            <input
-              id="fileInput"
-              type="file"
-              accept="image/*"
-              onChange={handleChange}
-            />
+          <div className="createPost__loggedInBottom">
+            <div className="createPost__imageUpload">
+              <label htmlFor="fileInput">
+                <AddAPhotoIcon style={{cursor: "pointer", fontSize: "20px"}}/>
+              </label>
+              <input
+                id="fileInput"
+                type="file"
+                accept="image/*"
+                onChange={handleChange}
+              />
+              <div className="createPost__imagePreview">
+                <img id="image_preview" alt="Post image"/>
+              </div>
+            </div>
+            <button
+              className="createPost__uploadBtn"
+              onClick={handleUpload}
+              style={{color: caption ? "#000" : "lightgrey"}}>
+                Upload
+            </button>
           </div>
         </div>)
         :
